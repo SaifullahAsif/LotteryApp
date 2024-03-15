6a1aafddd75130d7823e0e142c40548bed4909c5
SaifullahAsif <saifullahasif9000@gmail.com>
2024-03-15 15:26:55 +0500

    Made a lottery basic app

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,65 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+  int x = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _login(context);
-              },
-              child: Text('Login'),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 190, 13, 0),
+          title: const Center(
+            child: Text('Lottery App'),
+          ),
         ),
+        body: SafeArea(
+          child: x==4 ? const Center(
+            child:  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('You have won the lottery'),
+                Icon(Icons.check_box),
+                Text('Congratulations!')
+              ],
+            ),
+          ):const Center(
+            child:  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('The lucky number is: 4'),
+                Text('Better Luck Next Time!!'),
+              ],
+            ),
+          ),
+        ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        print('Pressed');
+        x = random.nextInt(10);
+        print(x);
+        setState(() {});
+      },
+      backgroundColor: const Color.fromARGB(255, 190, 13, 0),
+      child: const Icon(Icons.refresh), 
+      ),
       ),
     );
-  }
-
-  void _login(BuildContext context) async {
-    // Here's where you would integrate with your backend.
-    // For now, let's just print out the entered username and password.
-    print('Username: ${_usernameController.text}');
-    print('Password: ${_passwordController.text}');
-  }
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    super.dispose();
   }
 }
